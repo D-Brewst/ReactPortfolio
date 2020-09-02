039b4f4b16432075f269e4b597516259d186fd23
D-Brewst <devonbrewster31@gmail.com>
2020-09-01 23:57:38 -0400

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "../footer.css";
 
 function MyFooter(){
     return (
         <div className="footer mt-auto py-3">
             <p>
-                <a href="https://github.com/D-Brewst"><i class="fa fa-github" style="font-size:36px"></i></a>
-                <a href="https://www.linkedin.com/in/devon-brewster-3172a01b1/"><i class="fa fa-linkedin-square" style="font-size:36px"></i></a>
+                <a href="https://github.com/D-Brewst"><i class="fa fa-github" style={{fontSize: "36px"}}></i></a>
+                <a href="https://www.linkedin.com/in/devon-brewster-3172a01b1/"><i class="fa fa-linkedin-square" style={{fontSize:"36px"}}></i></a>
             </p>
             <p>
                 &copy; Devon Brewster, 2020

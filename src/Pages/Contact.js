99caf2c30ebe7f7d9c64350b5f79c6781cb5e2a4
D-Brewst <devonbrewster31@gmail.com>
2020-09-02 22:49:44 -0400

    Added contact page

---
+++ src/Pages/Contact.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Form} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Contact(){
+    return(
+        <Container style={{backgroundColor: "white"}}>
+            <Row>
+                <Col>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="email" placeholder="Your Name" />
+                    </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" placeholder="Enter your message here." />
+                    </Form.Group>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
+        
+            
+    )
+}
+
+export default Contact;
--- src/Routes.js
+++ src/Routes.js
@@ -4,20 +4,21 @@ import MyNavbar from "./components/Navbar";
 import MyFooter from "./components/Footer";
 import Portfolio from "./Pages/Portfolio";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 export default function Routes() {
   return (
     <Router>
       <MyNavbar />
       <Switch>
-        <Route path="/Portfolio">
-          <Portfolio />
-        </Route>
-        <Route path="/">
+        <Route exact path="/">
           <About />
         </Route>
-        <Route path="/Contact">
-          <h1>Contact</h1>
+        <Route exact path="/Portfolio">
+          <Portfolio />
+        </Route>
+        <Route exact path="/Contact">
+          <Contact />
         </Route>
       </Switch>
       <MyFooter />

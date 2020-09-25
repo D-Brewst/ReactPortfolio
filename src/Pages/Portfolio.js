42c4ee6f9e13ef24e0d870b3d3a199f4afd8bd9a
D-Brewst <devonbrewster31@gmail.com>
2020-09-25 14:50:23 -0400

    fixed image loading on portfolio page

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -4,8 +4,33 @@ import projects from "../projects.json";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import CodeQuiz from "../assets/Images/CodeQuiz.png";
+import thevillage from "../assets/Images/thevillage.png";
+import NUZlogo from "../assets/Images/NUZlogo.png";
+import WorkDayScheduler from "../assets/Images/WorkDayScheduler.png";
 
 function Portfolio(){
+    const image = {
+      CodeQuiz: {
+        src: CodeQuiz
+      },
+      thevillage: {
+        src: thevillage
+      },
+      NUZlogo: {
+        src: NUZlogo
+      },
+      WorkDayScheduler: {
+        src: WorkDayScheduler
+      },
+      PasswordGenerator: {
+        src: "http://www.innovins.com/wp-content/uploads/2017/03/Strong-Password.jpg"
+      },
+      WeatherDashboard: {
+        src: "https://wi-images.condecdn.net/image/doEYpG6Xd87/crop/2040/f/weather.jpg"
+      }
+    }
+
     return (
     <Container>
         <Row>
@@ -13,7 +38,7 @@ function Portfolio(){
           <Col md={4}>
             <Project 
                 title={project.title}
-                image={project.image}
+                image={image[project.image] ? image[project.image].src : "tree"}
                 link={project.link}
                 key={project.id} />
           </Col>))}
--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 
 function Project(props){
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card className="card h-100" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>

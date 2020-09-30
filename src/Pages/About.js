d34475ff6bedc235fe2a656d04298d3eddcebdf8
D-Brewst <devonbrewster31@gmail.com>
2020-09-29 20:53:58 -0400

    routed the buttons on the about page to thier respective pages

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,10 +4,15 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import pro from "../assets/Images/pro.JPG";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import { useHistory } from "react-router-dom";
 import "./about.css";
 
 function About(){
+    const history = useHistory();
+    const redirectPortfolio = () => history.push("/Portfolio");
+    const redirectContact = () => history.push("/Contact");
+
     return (
         <div>
             <Jumbotron fluid className="jumbo"></Jumbotron>
@@ -18,15 +23,15 @@ function About(){
                 <Row>
                     <Col>
                         <h1>I'm Devon, a Full Stack Developer.</h1>
-                        <p>Check out my work</p>
+                        <p>I am a recent coding bootcamp gradate, with a strong passion for code. I am looking for the opportunity be become apart of a great development team. Check out my work and feel free to contact me.</p>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Button id="portBtn" size="lg">
+                        <Button id="portBtn" size="lg" onClick={redirectPortfolio}>
                             My Portfolio
                         </Button>
-                        <Button id="tactBtn" size="lg">
+                        <Button id="tactBtn" size="lg" onClick={redirectContact}>
                             Contact Me
                         </Button>
                     </Col>

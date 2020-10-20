00827a1ca757cab47ed9bcf0601fe2fda5203d1e
D-Brewst <devonbrewster31@gmail.com>
2020-10-20 19:38:24 -0400

    created skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Skills(){
+    return (
+        <div class="skills">
+            <h1>My Skills</h1>
+            <li>
+                <h3>HTML</h3><span class="bar"><span class="html"></span></span>
+            </li>
+            <li>
+                <h3>CSS</h3><span class="bar"><span class="css"></span></span>
+            </li>
+            <li>
+                <h3>JavaScript</h3><span class="bar"><span class="javascript"></span></span>
+            </li>
+            <li>
+                <h3>jQuery</h3><span class="bar"><span class="jquery"></span></span>
+            </li>
+            <li>
+                <h3>React</h3><span class="bar"><span class="react"></span></span>
+            </li>
+            <li>
+                <h3>Node.js</h3><span class="bar"><span class="node"></span></span>
+            </li>
+            <li>
+                <h3>MySQL</h3><span class="bar"><span class="mysql"></span></span>
+            </li>
+            <li>
+                <h3>MongoDB</h3><span class="bar"><span class="mongo"></span></span>
+            </li>
+        </div>
+    )
+}
+
+export default Skills;

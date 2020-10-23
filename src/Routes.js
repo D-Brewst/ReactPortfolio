bcd7805fe1dfc30afd31eae90d6d43d888b139e0
D-Brewst <devonbrewster31@gmail.com>
2020-10-23 18:13:07 -0400

    changed Pages to pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,29 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import MyNavbar from "./components/Navbar";
 import MyFooter from "./components/Footer";
-import Portfolio from "./Pages/Portfolio";
-import Contact from "./Pages/Contact";
-import Home from "./Pages/Home";
+
+import Home from "./pages/Home";
 
 export default function Routes() {
   return (
     <Router>
       <MyNavbar />
-      <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        {/* <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/Portfolio">
-          <Portfolio />
-        </Route>
-        <Route exact path="/Contact">
-          <Contact />
-        </Route> */}
-      </Switch>
       <MyFooter />
     </Router>
   );

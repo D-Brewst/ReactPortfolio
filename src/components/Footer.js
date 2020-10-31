6ea7c08e71c430eedc9852e30998156c9fa9371f
D-Brewst <devonbrewster31@gmail.com>
2020-10-31 11:46:06 -0400

    fixed linkin icon link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function MyFooter(){
         <div className="footer mt-auto py-3">
             <p>
                 <a href="https://github.com/D-Brewst"><MDBIcon id="github" className="icons" fab icon="github" style={{fontSize:"36px"}} /></a>
-                <a href="https://linkedin.com/D-Brewst"><MDBIcon id="linkedin" className="icons" fab icon="linkedin" style={{fontSize:"36px"}} /></a>
+                <a href="https://linkedin.com/in/D-Brewst"><MDBIcon id="linkedin" className="icons" fab icon="linkedin" style={{fontSize:"36px"}} /></a>
             </p>
             <p>
                 &copy; Devon Brewster, 2020
